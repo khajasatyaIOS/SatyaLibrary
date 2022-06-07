11170a8337aeab6b3d4153b39aef4531d11ba5cf
Satya Kaja <khajasatyaios@gmail.com>
2022-06-07 22:46:24 +0530

    update uiview corner radius

--- Sources/SatyaLibrary/SatyaLibrary.swift
+++ Sources/SatyaLibrary/SatyaLibrary.swift
@@ -1,6 +1,14 @@
+import UIKit
 public struct SatyaLibrary {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+}
+
+extension UIView {
+    func setCornerRadius(radius: CGFloat) {
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
     }
 }
